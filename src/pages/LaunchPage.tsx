da7dcee2efe406ba5a3e29c520cc8a4d0a2783f0
Alison Wood <alison.wood@zonal.co.uk>
2021-04-13 21:21:16 +0100

    WIP for filter by year

--- src/pages/LaunchPage.tsx
+++ src/pages/LaunchPage.tsx
@@ -5,6 +5,7 @@ import {getAllLaunches} from '../api';
 
 import '../styles/LaunchPage.scss';
 
+import refreshIcon from '../assets/icon/refresh.png';
 import sortIcon from '../assets/icon/sort.png';
 import spacexLogo from '../assets/img/spacex-logo.png';
 import launchLogo from '../assets/img/launch-home.png';
@@ -19,6 +20,8 @@ export const LaunchPage = () => {
 
     const [launches, setLaunches] = useState<Array<LaunchItem>>([]);
     const [sortOrder, setSortOrder] = useState<string>('asc');
+    /* WIP for filter by year
+    const [yearsFilterList, setYearsFilterList] = useState<Array<>>([]); */
 
     const loadLaunches = async () => {
         const apiLaunches = await getAllLaunches();
@@ -34,11 +37,27 @@ export const LaunchPage = () => {
               launchItems.push(newLaunchItem);
         });
         setLaunches(launchItems);
+        /* part of WIP for filter by year
+        getYearsToFilterBy(launchItems); */
     };
 
     useEffect(() => {
         loadLaunches();
     }, []);
+
+    /* WIP: filter by year
+    const getYearsToFilterBy = ((launchItems: Array<LaunchItem>) => {
+        const launchYears = launchItems.map((launchItem: LaunchItem) => {
+            return launchItem.launchDate.getFullYear().toString();
+        }).filter((year, index, self) => {
+            return self.indexOf(year) === index;
+    });
+        const arrayYears = launchYears.map((year) => {
+            return {value: year, label: year}
+        })
+        setYearsFilterList(arrayYears);
+    });
+    */
 
     const changeSortOrder = () => {
         sortOrder === 'asc' ? setSortOrder('desc') : setSortOrder('asc');
@@ -61,28 +80,52 @@ export const LaunchPage = () => {
         });
     }
 
+    /* TODO: onChange function for select component
+    const onFilterChange = (e) => {
+        set selected year in state
+        filter launches where launchDate year === selected year
+        set filtered list of launches in state
+    } */
+
     return (
         <div className='launch-page'>
             <div className='launch-page-header'>
                 <img 
                     className='spacex-logo' 
                     src={spacexLogo} 
-                    alt='spacex title logo'
+                    alt='SpaceX Title Logo'
                     />
                 <button
+                    className='refresh-button'
+                    data-testid='refresh-button'
                     onClick={() => loadLaunches()}>
                     Reload data
+                    <img 
+                        className='refresh-icon' 
+                        src={refreshIcon} 
+                        alt='Refresh Icon'
+                    />
+
                 </button>
             </div>
 
             <div className='launch-page-content'>
                 <img className='launch-logo' src={launchLogo} alt='rocket launch logo'/>
-                <div>
-                    <button 
-                        onClick={changeSortOrder}>
-                        Sort {sortOrder === 'asc' ? 'Descending' : 'Ascending'}
-                        <img className='sortIcoon' src={sortIcon} alt='up and down sort icons'/>
-                    </button>
+
+                <div className='launch-list-div'>
+                    <div className='button-div'>
+                        {/* TODO: Select will go here  */}
+                        <button
+                            className='sort-button'
+                            onClick={changeSortOrder}>
+                            <span>Sort {sortOrder === 'asc' ? 'Descending' : 'Ascending'}</span>
+                            <img 
+                                className='sort-icon' 
+                                src={sortIcon} 
+                                alt='Up and Down Sort Arrows'
+                            />
+                        </button>
+                    </div>
                 <LaunchList launches={launches}/>
                 </div>
            </div>
